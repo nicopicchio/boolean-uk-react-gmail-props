85ada5fe28b9d74943bb0f129f9b2ed543815ada
Nico Picchio <nicolapicchio@gmail.com>
2022-03-01 15:25:50 +0000

    first commit

---
+++ src/Emails.js
@@ -0,0 +1,30 @@
+function Emails(props) {
+	return (
+		<ul>
+			{props.filteredEmails.map((email, index) => (
+				<li key={index} className={`email ${email.read ? 'read' : 'unread'}`}>
+					<div className="select">
+						<input
+							className="select-checkbox"
+							type="checkbox"
+							checked={email.read}
+							onChange={() => toggleRead(email)}
+						/>
+					</div>
+					<div className="star">
+						<input
+							className="star-checkbox"
+							type="checkbox"
+							checked={email.starred}
+							onChange={() => toggleStar(email)}
+						/>
+					</div>
+					<div className="sender">{email.sender}</div>
+					<div className="title">{email.title}</div>
+				</li>
+			))}
+		</ul>
+	);
+}
+
+export default Emails
--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ import initialEmails from './data/emails'
 
 import './styles/app.css'
 
+import Emails from './Emails'
+
 const getReadEmails = emails => emails.filter(email => !email.read)
 
 const getStarredEmails = emails => emails.filter(email => email.starred)
@@ -88,33 +90,7 @@ function App() {
         </ul>
       </nav>
       <main className="emails">
-        <ul>
-          {filteredEmails.map((email, index) => (
-            <li
-              key={index}
-              className={`email ${email.read ? 'read' : 'unread'}`}
-            >
-              <div className="select">
-                <input
-                  className="select-checkbox"
-                  type="checkbox"
-                  checked={email.read}
-                  onChange={() => toggleRead(email)}
-                />
-              </div>
-              <div className="star">
-                <input
-                  className="star-checkbox"
-                  type="checkbox"
-                  checked={email.starred}
-                  onChange={() => toggleStar(email)}
-                />
-              </div>
-              <div className="sender">{email.sender}</div>
-              <div className="title">{email.title}</div>
-            </li>
-          ))}
-        </ul>
+        <Emails />
       </main>
     </div>
   )
