0d3f973aad8eb274765c8d11d922887536f0ac13
Nico Picchio <nicolapicchio@gmail.com>
2022-03-01 16:15:25 +0000

    core criteria completed

--- src/Emails.js
+++ src/Emails.js
@@ -1,30 +1,18 @@
+import Email from './Email';
+
 function Emails(props) {
 	return (
 		<ul>
 			{props.filteredEmails.map((email, index) => (
-				<li key={index} className={`email ${email.read ? 'read' : 'unread'}`}>
-					<div className="select">
-						<input
-							className="select-checkbox"
-							type="checkbox"
-							checked={email.read}
-							onChange={() => props.toggleRead(email)}
-						/>
-					</div>
-					<div className="star">
-						<input
-							className="star-checkbox"
-							type="checkbox"
-							checked={email.starred}
-							onChange={() => props.toggleStar(email)}
-						/>
-					</div>
-					<div className="sender">{email.sender}</div>
-					<div className="title">{email.title}</div>
-				</li>
+				<Email
+					email={email}
+					index={index}
+					toggleRead={props.toggleRead}
+					toggleStar={props.toggleStar}
+				/>
 			))}
 		</ul>
 	);
 }
 
-export default Emails
+export default Emails;
