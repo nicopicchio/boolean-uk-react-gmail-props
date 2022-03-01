eef72d6edf8f273d9f2d029b8fa8fbf9038f655f
Nico Picchio <nicolapicchio@gmail.com>
2022-03-01 18:09:25 +0000

    final submission

--- src/EmailRender.js
+++ src/EmailRender.js
@@ -1,3 +1,11 @@
-function EmailRender() {}
+function EmailRender(props) {
+  return (
+    <div className="email-container">
+      <h3>{`From: ${props.email.sender}`}</h3>
+      <h3>{`From: ${props.email.title}`}</h3>
+      <p>{`${props.email.content}`}</p>
+    </div>
+  )
+}
 
 export default EmailRender;
